650a77f0ab1b93caa2860b09672550158324439b
John Källström <johnkaellstrom@gmail.com>
2024-06-07 16:53:47 +0200

    Display machines

--- MachinePark/Features/Machines/Components/TableOfMachines.razor.cs
+++ MachinePark/Features/Machines/Components/TableOfMachines.razor.cs
@@ -1,6 +1,11 @@
-﻿namespace MachinePark.Features.Machines.Components
+﻿using MachinePark.ViewModels;
+using Microsoft.AspNetCore.Components;
+
+namespace MachinePark.Features.Machines.Components
 {
     public partial class TableOfMachines
     {
+        [Parameter]
+        public IEnumerable<MachineViewModel> Model { get; set; } = [];
     }
 }
---
+++ MachinePark/ViewModels/MachineViewModel.cs
@@ -0,0 +1,12 @@
+﻿using MachinePark.Domain.Enums;
+
+namespace MachinePark.ViewModels
+{
+    public record MachineViewModel
+    {
+        public Guid Id { get; init; }
+        public string? Name { get; init; }
+        public MachineStatus Status { get; init; }
+        public string? Type { get; init; }
+    }
+}
