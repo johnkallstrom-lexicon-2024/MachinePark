8cb49be133d34b83bc131d84455dd985fc12f3cb
John Källström <johnkaellstrom@gmail.com>
2024-06-10 16:59:24 +0200

    Add api proj

---
+++ MachinePark.Api/Controllers/MachinesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MachinePark.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MachinesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id:guid}")]
+        public IActionResult GetById(Guid id)
+        {
+            return Ok();
+        }
+    }
+}
