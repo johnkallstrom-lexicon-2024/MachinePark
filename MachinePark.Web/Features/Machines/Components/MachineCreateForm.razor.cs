12c5ead63cbf8eb04f06eb252b8f9879257d67c3
John Källström <johnkaellstrom@gmail.com>
2024-06-10 16:53:12 +0200

    Select options

---
+++ MachinePark.Web/Features/Machines/Components/MachineCreateForm.razor.cs
@@ -0,0 +1,20 @@
+﻿using MachinePark.Web.Features.Machines.ViewModels;
+using Microsoft.AspNetCore.Components;
+
+namespace MachinePark.Web.Features.Machines.Components
+{
+    public partial class MachineCreateForm
+    {
+        [Parameter]
+        public string? Title { get; set; }
+
+        public MachineCreateViewModel Model { get; set; } = new();
+
+        private async Task Submit()
+        {
+            Console.WriteLine($"Name: {Model.Name}");
+            Console.WriteLine($"Status: {Model.Status}");
+            Console.WriteLine($"MachineTypeId: {Model.MachineTypeId}");
+        }
+    }
+}
