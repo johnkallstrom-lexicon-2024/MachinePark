49dc5dd1000c30f9670e90df4dfae227d9a9635f
John Källström <johnkaellstrom@gmail.com>
2024-06-13 17:02:52 +0200

    Add spinner for loading

--- MachinePark.Web/Features/Machines/Components/MachineTable.razor.cs
+++ MachinePark.Web/Features/Machines/Components/MachineTable.razor.cs
@@ -5,33 +5,35 @@
         [Inject]
         public IHttpService HttpService { get; set; } = default!;
 
-        public IEnumerable<MachineDto> Model { get; set; } = [];
+        public IEnumerable<MachineDto> Model { get; set; } = default!;
 
+        public bool Loading { get; set; } = false;
         public string[] Errors { get; set; } = default!;
 
         protected override async Task OnInitializedAsync()
         {
-            var result = await HttpService.GetAsync<IEnumerable<MachineDto>>(Endpoints.Machines);
-            if (result.Succeeded)
-            {
-                Model = result.Data;
-            }
-            else
-            {
-                Errors = result.Errors;
-            }
+            await GetMachines();
         }
 
         private async Task Refresh()
         {
+            await GetMachines();
+        }
+
+        private async Task GetMachines()
+        {
+            Loading = true;
+
             var result = await HttpService.GetAsync<IEnumerable<MachineDto>>(Endpoints.Machines);
             if (result.Succeeded)
             {
                 Model = result.Data;
+                Loading = false;
             }
             else
             {
                 Errors = result.Errors;
+                Loading = false;
             }
         }
     }
--- MachinePark.Web/Features/Statistics/Components/DailyStatistics.razor.cs
+++ MachinePark.Web/Features/Statistics/Components/DailyStatistics.razor.cs
@@ -7,6 +7,9 @@
 
         public StatisticsDto Model { get; set; } = default!;
 
+        public bool Loading { get; set; } = false;
+        public string[] Errors { get; set; } = default!;
+
         protected override async Task OnInitializedAsync()
         {
             await GetStatistics();
@@ -19,10 +22,18 @@
 
         private async Task GetStatistics()
         {
+            Loading = true;
+
             var result = await HttpService.GetAsync<StatisticsDto>(Endpoints.Statistics);
             if (result.Succeeded)
             {
                 Model = result.Data;
+                Loading = false;
+            }
+            else
+            {
+                Errors = result.Errors;
+                Loading = false;
             }
         }
     }
