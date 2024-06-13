c2f68a0bcfb181ac48f9ce51ea3b6bbfaa89de70
John Källström <johnkaellstrom@gmail.com>
2024-06-13 16:11:37 +0200

    UI changes + refresh buttn

---
+++ MachinePark.Web/Features/Machines/Components/MachineTable.razor.cs
@@ -0,0 +1,20 @@
+﻿namespace MachinePark.Web.Features.Machines.Components
+{
+    public partial class MachineTable
+    {
+        [Inject]
+        public IHttpService HttpService { get; set; } = default!;
+
+        [Parameter]
+        public IEnumerable<MachineDto> Model { get; set; } = [];
+
+        private async Task Refresh()
+        {
+            var result = await HttpService.GetAsync<IEnumerable<MachineDto>>(Endpoints.Machines);
+            if (result.Succeeded)
+            {
+                Model = result.Data;
+            }
+        }
+    }
+}
