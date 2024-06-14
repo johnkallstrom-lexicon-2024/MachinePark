ac0fe12d9060567397ea325e7ba333107f693e3b
John Källström <johnkaellstrom@gmail.com>
2024-06-14 16:48:38 +0200

    Simplifed http service/wrapper

---
+++ MachinePark.Web/Services/HttpService.cs
@@ -0,0 +1,31 @@
+﻿namespace MachinePark.Web.Services
+{
+    public class HttpService : IHttpService
+    {
+        private readonly HttpClient _httpClient;
+
+        public HttpService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<TData?> GetAsync<TData>(string url)
+        {
+            TData? data = default;
+
+            var httpResponse = await _httpClient.GetAsync(url);
+            if (httpResponse.IsSuccessStatusCode)
+            {
+                data = await httpResponse.Content.ReadFromJsonAsync<TData>();
+            }
+
+            return data;
+        }
+
+        public async Task PostAsync(string url, object value) => await _httpClient.PostAsJsonAsync(url, value);
+
+        public async Task PutAsync(string url, object value) => await _httpClient.PutAsJsonAsync(url, value);
+
+        public async Task DeleteAsync(string url) => await _httpClient.DeleteAsync(url);
+    }
+}
---
+++ MachinePark.Web/Services/IHttpService.cs
@@ -0,0 +1,10 @@
+﻿namespace MachinePark.Web.Services
+{
+    public interface IHttpService
+    {
+        Task<TData?> GetAsync<TData>(string url);
+        Task PostAsync(string url, object value);
+        Task PutAsync(string url, object value);
+        Task DeleteAsync(string url);
+    }
+}
